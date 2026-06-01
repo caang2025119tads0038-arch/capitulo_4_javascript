abc04e469053a73164856a7034a3aff6ee2bfc31
Guilherme Nunes <caang.2025119tads0038@aluno.ifpi.edu.br>
2026-06-01 11:01:13 -0300

    continuação do resumo e começo do design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+function testarAlert(){
+    alert("Esta mensagem usa a função 'alert()'!")
+}
 
+function testarConfirm() {
+    confirm("Esta mensagem usa a função 'confirm()'!")
+}
+
+function testarPrompt() {
+    prompt("Esta mensagem usa a função 'prompt()'!")
+}
